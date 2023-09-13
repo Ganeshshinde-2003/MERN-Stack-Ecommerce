8918f2bc0711b58a67c419edbef64186003a9155
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-13 14:34:56 +0530

    created navbar

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink to="/" className="navbar-brand" href="#">
+              Hidden brand
+            </NavLink>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" href="#">
+                  Link
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className="nav-link disabled"
+                  aria-disabled="true"
+                >
+                  Disabled
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
