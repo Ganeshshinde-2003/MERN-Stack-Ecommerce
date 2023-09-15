c396f3995ffc1484c520f3bffba6d96ba33cbef7
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-15 11:46:29 +0530

    created the login and signup page

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -13,10 +13,13 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         navigate("/");
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/register", {
+      const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`, {
         name,
         email,
         password,
