6e1995288667b58b1f98f08c779f92c223d621c7
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-07 20:08:27 +0530

    updated

--- server.js
+++ server.js
@@ -7,25 +7,29 @@ import authRouter from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRouter from "./routes/categoryRoute.js";
 import productRouter from "./routes/productRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/product", productRouter);
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "welcome to my sitee",
-  });
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
