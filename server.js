dbe1d92949d3b93187d790f3c754ba4147762b5b
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-07 20:10:37 +0530

    created the updates

--- server.js
+++ server.js
@@ -7,29 +7,25 @@ import authRouter from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRouter from "./routes/categoryRoute.js";
 import productRouter from "./routes/productRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
 connectDB();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/product", productRouter);
 
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", (req, res) => {
+  res.send({
+    message: "welcome to my sitee",
+  });
 });
 
 const PORT = process.env.PORT || 8080;
